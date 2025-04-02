2caaba88d1b32bdf343166d9ed6fb4874294f081
admin <jhlee050302@gmail.com>
2025-04-02 11:28:13 +0900

    5주차 수업

---
+++ week5/js/js_test.js
@@ -0,0 +1,16 @@
+
+if (true){
+    let c = 'let  접근';
+    var c_1= 'var 접근';
+}
+
+console.log(c_1);
+
+let d = 5;
+//d = '값 재할당'; //Error
+console.log(d);
+
+const e = '상수1 접근';
+// e = 5;// Error
+// const f; //Error
+console.log(e);
