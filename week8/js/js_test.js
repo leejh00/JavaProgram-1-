2a5826e9c3d12514521c2bb7a60edd15633970d8
admin <jhlee050302@gmail.com>
2025-04-30 10:48:36 +0900

    9주차

---
+++ week8/js/js_test.js
@@ -0,0 +1,32 @@
+
+if (true){
+    let c = 'let  접근';
+    var c_1= 'var 접근';
+}
+
+console.log(c_1);
+
+let d = 5;
+//d = '값 재할당'; //Error
+console.log(d);
+
+const e = '상수1 접근';
+// e = 5;// Error
+// const f; //Error
+console.log(e);
+
+
+
+
+//데이터 타입
+let number = 5;
+let str = '문자열 입력';//""도 가능
+let prime = 1.3111;
+let is_ok = true;
+let is_not = false;
+let underfi; //변수 이름만, 초기화 x
+let empty = null; //비어있음
+
+console.log(underfi, empty);//여러개 출력
+
+
